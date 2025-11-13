0498bf93f7957d6360412ed28b3cfe800d401538
JORGE IGNACIO PEREZ FLOREZ <moabencod3@gmail.com>
2025-11-13 12:51:48 +0100

    arreglo de movimiento + freze vertical MainCharacter

--- Assets/Scripts/Top Down Character Controller.cs
+++ Assets/Scripts/Top Down Character Controller.cs
@@ -6,45 +6,57 @@ namespace Cainos.PixelArtTopDown_Basic
 {
     public class TopDownCharacterController : MonoBehaviour
     {
-        public float speed;
+        public float speed = 5f;
 
         private Animator animator;
+        private Rigidbody2D rb;
 
         private void Start()
         {
             animator = GetComponent<Animator>();
+            rb = GetComponent<Rigidbody2D>();
         }
-
 
         private void Update()
         {
             Vector2 dir = Vector2.zero;
+
+            // Movimiento lateral
             if (Input.GetKey(KeyCode.A))
             {
                 dir.x = -1;
-                animator.SetInteger("Direction", 3);
+                animator.SetInteger("Direction", 3); // Izquierda
             }
             else if (Input.GetKey(KeyCode.D))
             {
                 dir.x = 1;
-                animator.SetInteger("Direction", 2);
+                animator.SetInteger("Direction", 2); // Derecha
             }
 
+            // Movimiento vertical
             if (Input.GetKey(KeyCode.W))
             {
                 dir.y = 1;
-                animator.SetInteger("Direction", 1);
+                animator.SetInteger("Direction", 1); // Arriba
             }
             else if (Input.GetKey(KeyCode.S))
             {
                 dir.y = -1;
-                animator.SetInteger("Direction", 0);
+                animator.SetInteger("Direction", 0); // Abajo
             }
 
+            // Normalizar y aplicar movimiento
             dir.Normalize();
             animator.SetBool("IsMoving", dir.magnitude > 0);
+        }
 
-            GetComponent<Rigidbody2D>().linearVelocity = speed * dir;
+        private void FixedUpdate()
+        {
+            rb.linearVelocity = speed * new Vector2(
+                Input.GetAxisRaw("Horizontal"),
+                Input.GetAxisRaw("Vertical")
+            ).normalized;
+            
         }
     }
 }
