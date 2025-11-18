2babc7cff1d319a4a89329ad4f99551576e4a009
JORGE IGNACIO PEREZ FLOREZ <moabencod3@gmail.com>
2025-11-18 08:31:23 +0100

    aniamciones

--- Assets/Scripts/Top Down Character Controller.cs
+++ Assets/Scripts/Top Down Character Controller.cs
@@ -6,57 +6,49 @@ namespace Cainos.PixelArtTopDown_Basic
 {
     public class TopDownCharacterController : MonoBehaviour
     {
-        public float speed = 5f;
+        public float speed;
 
         private Animator animator;
-        private Rigidbody2D rb;
 
         private void Start()
         {
             animator = GetComponent<Animator>();
-            rb = GetComponent<Rigidbody2D>();
         }
 
         private void Update()
         {
             Vector2 dir = Vector2.zero;
 
-            // Movimiento lateral
+            // Movimiento horizontal
             if (Input.GetKey(KeyCode.A))
             {
                 dir.x = -1;
-                animator.SetInteger("Direction", 3); // Izquierda
+                animator.SetInteger("Direction", 3);   // Izquierda
             }
             else if (Input.GetKey(KeyCode.D))
             {
                 dir.x = 1;
-                animator.SetInteger("Direction", 2); // Derecha
+                animator.SetInteger("Direction", 2);   // Derecha
             }
 
             // Movimiento vertical
             if (Input.GetKey(KeyCode.W))
             {
                 dir.y = 1;
-                animator.SetInteger("Direction", 1); // Arriba
+                animator.SetInteger("Direction", 1);   // Arriba
             }
             else if (Input.GetKey(KeyCode.S))
             {
                 dir.y = -1;
-                animator.SetInteger("Direction", 0); // Abajo
+                animator.SetInteger("Direction", 0);   // Abajo
             }
 
-            // Normalizar y aplicar movimiento
             dir.Normalize();
-            animator.SetBool("IsMoving", dir.magnitude > 0);
-        }
 
-        private void FixedUpdate()
-        {
-            rb.linearVelocity = speed * new Vector2(
-                Input.GetAxisRaw("Horizontal"),
-                Input.GetAxisRaw("Vertical")
-            ).normalized;
-            
+            animator.SetBool("IsMoving", dir.magnitude > 0);
+
+            // Movimiento real del Rigidbody2D
+            GetComponent<Rigidbody2D>().linearVelocity = speed * dir;
         }
     }
 }
